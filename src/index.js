0f83094d5b1cbc1d42fd32146c87cb6f86bd1f50
zsj <zoushijun@threatbook.cn>
2020-12-25 20:32:06 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-const MyKoa = require("./app.js");
+export { default as Box } from "./components/Box";
 
-const app = new MyKoa();
-app.use(async (req, res, next) => {
-  console.log("1");
-  await next(); // 调用下一个middleware
-  console.log("5");
-});
-app.use(async (req, res, next) => {
-  console.log("2");
-  await next(); // 调用下一个middleware
-  console.log("4");
-});
-app.use(async (req, res, next) => {
-  console.log("3");
-  // res.end("hello");
-  // res.res.end("hello world");
-});
-// 监听端口号
-app.listen(3011, err => {
-  if (!err) {
-    console.log("server is starting");
-  } else {
-    console.log(err);
-  }
-});
+require("./styles/index");
