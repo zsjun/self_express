6190003ce8ead79e42958210f7f8202fc2a7ee7c
zsj <zoushijun@threatbook.cn>
2021-02-04 15:57:42 +0800

    add src/components

---
+++ src/components/alert/alter.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { AlertProps, KindMap } from "./interface";
+
+const prefixCls = "happy-alert";
+
+const kinds: KindMap = {
+  info: "#5352ED",
+  positive: "#2ED573",
+  negative: "#FF4757",
+  warning: "#FFA502",
+};
+
+const Alert: React.FC<AlertProps> = ({ children, kind = "info", ...rest }) => (
+  <div
+    className={prefixCls}
+    style={{
+      background: kinds[kind],
+    }}
+    {...rest}
+  >
+    {children}
+  </div>
+);
+
+export default Alert;
